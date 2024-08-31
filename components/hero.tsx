a7e7dfb11a7717abc469de280cfcf2c6eb6ca1c4
r <r.thomas8@lancaster.ac.uk>
2024-08-31 14:07:41 +0100

    Changed some text

--- components/hero.tsx
+++ components/hero.tsx
@@ -16,12 +16,12 @@ const Hero = () => {
           <div className="flex justify-center relative my-20 z-10">
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
               <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                Text
+                Finally making one?!
               </h2>
 
               <TextGenerateEffect
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Portfolio Placeholder"
+                words="Portfolio Party Time"
               />
               <p>
                 Hi, I'm Ryan, an API Developer based in the UK
