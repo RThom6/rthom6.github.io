0e69dd91c0e46044c995e784cec246aef1a6ad75
r <r.thomas8@lancaster.ac.uk>
2024-08-31 23:30:39 +0100

    testing feature

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ import { FaGithub, FaLinkedin, FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className="pt-10">
+    <div>
         <div className="h-screen w-full dark:bg-black bg-white flex items-center justify-center relative">
         <HeroHighlight containerClassName='w-screen'>
           <div className="flex justify-center z-10">
