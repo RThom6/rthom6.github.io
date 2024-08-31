15ca164fde54b74a46032e5576fb74c730736596
r <r.thomas8@lancaster.ac.uk>
2024-08-31 19:08:05 +0100

    Added tello-rust and minecraft plugin description

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,10 @@
 export const gridItems = [
-    {title: 'Title1', description: 'Desc1', id: 1}
+    {title: 'tello-rust API', description: 'This project is designed to allow users to control a Ryze Tello drone using Rust. It provides a Rust-based interface to abstract away from the Tello SDK allowing for quick setup and works over a UDP port. This project demonstrates skills such as network programming, concurrency and use of data structures.', id: 1},
+    {title: 'Minecraft Plugin Development', description: 'Peer programmed multiple Java Minecraft plugins from scratch', id: 2},
+    {title: 'Title1', description: 'Desc1', id: 3},
+    {title: 'Title1', description: 'Desc1', id: 4},
+    {title: 'Title1', description: 'Desc1', id: 5},
+    {title: 'Title1', description: 'Desc1', id: 6},
+    {title: 'Title1', description: 'Desc1', id: 7},
+    {title: 'Title1', description: 'Desc1', id: 8},
 ]
