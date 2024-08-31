73353f8cea318578d6b59bc004884b344738cf64
r <r.thomas8@lancaster.ac.uk>
2024-08-31 23:58:40 +0100

    h-dvh

--- components/ui/hero-highlight.tsx
+++ components/ui/hero-highlight.tsx
@@ -29,7 +29,7 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative lg:h-[60rem] sm:h-[40rem] flex items-center bg-white dark:bg-black justify-center group",
+        "relative lg:h-[60rem] md:h-[50rem] sm:h-[40rem] flex items-center bg-white dark:bg-black justify-center group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
