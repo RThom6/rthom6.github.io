9180c3bd05c3d5420a0920c17d03f2db46ff7ae4
r <r.thomas8@lancaster.ac.uk>
2024-08-31 21:16:21 +0100

    Finally got the spacing right on large

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="bg-black-200 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="w-full">
         <FloatingNav navItems={[
           {name: 'Home', link:'/', icon: <FaHome/>}
         ]}/>
--- components/hero.tsx
+++ components/hero.tsx
@@ -7,14 +7,11 @@ import { FaGithub, FaLinkedin, FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-10">
-
-        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.0] bg-grid-black-100/[0.2] flex items-center justify-center absolute top-0 left-0">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        </div>
-        <HeroHighlight>
-          <div className="flex justify-center relative my-20 z-10">
-            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+    <div>
+        <div className="h-screen w-full dark:bg-black bg-white flex items-center justify-center relative">
+        <HeroHighlight containerClassName='w-screen'>
+          <div className="flex justify-center z-10">
+            <div className='md:max-w-2xl flex flex-col items-center justify-center'>
               <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                 Welcome to my portfolio
               </h2>
@@ -43,6 +40,7 @@ const Hero = () => {
             </div>
           </div>
         </HeroHighlight>
+      </div>
     </div>
   )
 }
